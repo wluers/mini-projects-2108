77582cb03bf37f674edc397e84056e786d88670c
wluers <wluers@gmail.com>
2020-08-25 12:08:15 -0700

    A bunch o stuff

---
+++ regexp.js
@@ -0,0 +1,75 @@
+// TO BREAK A LINE AFTER A PERIOD
+// find all periods: \.
+// replace with: \.\n 
+
+// TO ADD QUOTE AT START OF LINES",
+// find beginning of line: ^
+// replace with: "
+
+// TO ADD QUOTE AND COMMA AT END OF LINES",
+// find end of line: $
+// replace with: ",
+
+// TO REMOVE ALL LINE BREAKS 
+// find: \n\s+
+// replace with nothing
+
+let someText = [
+"You say yes, I say no",
+"You say stop and I say go, go, go",
+"Oh, no",
+"You say goodbye and I say hello",
+"Hello, hello",
+"I don't know why you say goodbye",
+"I say hello",
+"Hello, hello",
+"I don't know why you say goodbye",
+"I say hello",
+"I say high, you say low",
+"You say why and I say I don't know",
+"Oh, no",
+"You say goodbye and I say hello",
+"Hello, hello",
+"(Hello goodbye)",
+"I don't know why you say goodbye",
+"I say hello",
+"Hello, hello",
+"(Hello goodbye)",
+"I don't know why you say goodbye",
+"(Hello goodbye)",
+"I say hello",
+"Why, why, why, why, why, why",
+"Do you say goodbye?",
+"Goodbye, bye, bye, bye, bye",
+"Oh, no",
+"You say goodbye and I say hello",
+"Hello, hello",
+"I don't know why you say goodbye",
+"I say hello",
+"Hello, hello",
+"I don't know why you say goodbye",
+"I say hello",
+"You say yes",
+"(I say yes)",
+"I say no",
+"(But I may mean no)",
+"You say stop",
+"(I can stay)",
+"And I say go, go, go",
+"(Til it's time to go)",
+"Oh, no",
+"You say goodbye and I say hello",
+"Hello, hello",
+"I don't know why you say goodbye",
+"I say hello",
+"Hello, hello",
+"I don't know why you say goodbye",
+"I say hello",
+"Hello, hello",
+"I don't know why you say goodbye I say hello",
+"Hello",
+"Hela, goodbye hello",
+"Hela, goodbye hello",
+"Hela, heba helloa",
+
+ ];
