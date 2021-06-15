a0c310ce70d7cc76d932d01c4544f1940a99762f
wluers <wluers@gmail.com>
2021-06-15 16:20:26 -0700

    various changes

--- p5js-template/sketch.js
+++ p5js-template/sketch.js
@@ -1,24 +1,25 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  // set background color as greyscale integer: background(0) or with rgb: background(r,g,b);
-  background(220) 
+  //background(0);
 
   // create an ellipse ellipse(x, y, w, [h]) (height is optional)
   ellipse(50,50,80,80);
 
   //make a line: line(x1, y1, x2, y2)
-  line(15, 25, 70, 90);
+  line(15, 55, 70, 90);
 
-   // draw continuous circles. alter color with mouse down
-/*  if (mouseIsPressed) {
+
+  if (mouseIsPressed) {
     fill(0);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);*/
-
-
+  ellipse(mouseX, mouseY, 80, 80);
+  
 }
+
+
+
